5aa44ae8f1b572a9fb21997953b3b5d31cbc0f6d
Patrick Goldinger <patrick.goldinger.02@gmail.com>
2019-11-18 23:20:21 +0100

    Updated wiringPiSim to work on Windows.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # simpi
-A tool for simulating WiringPi projects on Windows and GNU/Linux.
+A tool for simulating wiringPi projects written for the Raspberry Pi on
+Windows and GNU/Linux.
 
-*NOTE: this project is currently in alpha version and therefore not
-stable!*
+*NOTE: this project is currently in alpha version and therefore only limited 
+features of wiringPi are available!*
 
 ## SimPi Broker
 
@@ -13,8 +14,7 @@ Raspberry Pi as well as the front-end user interface for the browser.
 
 1. Change to the directory `simpi_broker`
 2. Execute `make build` in the shell or cmd
-3. Locate the `simpi_broker.exe` or `simpi_broker` in the `.\out` dir and run
-    it.
+3. Execute `make run` in the shell or cmd to run the SimPi Broker
 4. Open [`http://127.0.0.1:32000`](http://127.0.0.1:32000) in your browser
     (Firefox, Chromium-based or Safari only!!)
 
@@ -22,16 +22,20 @@ Raspberry Pi as well as the front-end user interface for the browser.
 
 This is the library part of the library wiringPi which emulates to be the real
 library, but instead just communicates with the broker. The header file
-`wiringPi.h` is the original one of the library and not modified.
+`wiringPi.h` is the original one of the wiringPi library and has not been
+modified.
 
 ### How to build and run
 
 1. Change to the directory `simpi_wiringpi`
 2. Execute `make build-wiring-pi-sim` in the shell or cmd to build the library
     for your system
-2. Execute `make build` in the shell or cmd to build the executable of your
+3. Locate the `make.bat` or `makefile` and change the `IN_SRC_FILES` or `SRC`
+    env variable to the file you want to compile
+4. Execute `make build` in the shell or cmd to build the executable of your
     file
-3. Locate the `wpi_test.exe` or `wpi_test` in the `.\out` dir and run it.
+5. Make sure you have started the SimPi Broker
+6. Execute `make run` in the shell or cmd to run your program
 
 ## Used libraries and fonts
 - [wiringPi](https://github.com/WiringPi/WiringPi)
--- simpi_wiringpi/lib/wiringPi.c
+++ simpi_wiringpi/lib/wiringPi.c
@@ -8,25 +8,30 @@
  * License: MIT
  */
 
+#define _DEFAULT_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include "wiringPi.h"
-
-#define BUFSIZE 512
+#include <stdint.h>
 
 #if defined(_WIN32) || defined(_WIN64)
-    #define popen(c1, c2) _popen(c1, c2)
-    #define pclose(c1) _pclose(c1)
+    #define PLATFORM__WIN32
+    #include <winsock2.h>
+    #include <windows.h>
+    #pragma comment(lib,"ws2_32.lib") // Winsock Library
 #elif defined(__linux__) || defined(__linux) || defined(linux)
+    #define PLATFORM__LINUX
     #include <unistd.h>
+    #include <sys/time.h>
     #include <sys/socket.h>
     #include <sys/signal.h>
     #include <netinet/in.h>
     #include <netinet/tcp.h>
-    #include <fcntl.h>
+    #include <arpa/inet.h>
 #endif
+
+#include "wiringPi.h"
 
 #define HTTP_HEADER_GET "GET %s HTTP/1.1\r\nHost: 127.0.0.1:32000\r\nAccept: text/*\r\nContent-Length: 0\r\nConnection: close\r\n\r\n"
 #define HOST "127.0.0.1"
@@ -41,20 +46,36 @@ struct __ret_data_t {
     char value[16];
 };
 
+uint64_t __get_sys_time(void) {
+    uint64_t ret_time_us_raw;
+#if defined(PLATFORM__WIN32)
+    FILETIME curr_time;
+    // Gets time in 100ns precision (0.1us)
+    GetSystemTimePreciseAsFileTime(&curr_time);
+    ret_time_us_raw |= curr_time.dwHighDateTime;
+    ret_time_us_raw <<= 32;
+    ret_time_us_raw |= curr_time.dwLowDateTime;
+    // January 1st, 1970 - January 1st, 1601 UTC ~ 369 years
+    // or 116444736000000000 us
+    uint32_t ret_time_us = (uint32_t)(ret_time_us_raw - 116444736000000000);
+    ret_time_us /= (uint32_t)10; 
+#elif defined(PLATFORM__LINUX)
+    struct timeval curr_time;
+    gettimeofday(&curr_time, NULL);
+    ret_time_us = curr_time.tv_sec * (int)1e6 + curr_time.tv_usec;
+#endif
+return ret_time_us;
+}
+
 int __str_starts_with(const char* a, const char* b) {
    if (strncmp(a, b, strlen(b)) == 0) return 1;
    return 0;
 }
 
-int sock;
-struct sockaddr_in server;
-char message[256];
-char server_reply[1024];
-
 int __recv(int __fd, void *__buf, size_t __n, int __flags) {
     size_t br = 0, br_temp;
     while (br < __n) {
-        br_temp = recv(__fd, __buf + br, 1, __flags);
+        br_temp = recv(__fd, (char *)__buf + br, 1, __flags);
         if (br_temp == 0) {
             return 0;
         } else if (br_temp < 0) {
@@ -67,9 +88,21 @@ int __recv(int __fd, void *__buf, size_t __n, int __flags) {
 }
 
 struct __ret_data_t __request(char *cmd) {
+    int sock;
+    struct sockaddr_in server;
+    char message[256];
+    char server_reply[1024];
     struct __ret_data_t ret = {
         -1, "", "fail", "-1"
     };
+
+#if defined(PLATFORM__WIN32)
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0) {
+		fprintf(stderr, "[ERR] wiringPiSim: Failed to create socket. (%d)\n", WSAGetLastError());
+		return ret;
+	}
+#endif
 
     // Create socket
     sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -79,7 +112,11 @@ struct __ret_data_t __request(char *cmd) {
     }
 
     int nd = 1;
+#if defined(PLATFORM__WIN32)
+    if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (char *)&nd, sizeof(nd)) < 0) {
+#elif defined(PLATFORM__LINUX)
     if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &nd, sizeof(nd)) < 0) {
+#endif
         fputs("[ERR] wiringPiSim: Failed to set socket opt TCP_NODELAY.\n", stderr);
         return ret;
     }
@@ -107,7 +144,13 @@ struct __ret_data_t __request(char *cmd) {
 		fputs("[ERR] wiringPiSim: recv() from socket failed.\n", stderr);
         return ret;
 	}
+
+#if defined(PLATFORM__WIN32)
+    closesocket(sock);
+    WSACleanup();
+#elif defined(PLATFORM__LINUX)
     close(sock);
+#endif
 
     char *body = strstr(server_reply, "\r\n\r\n");
     if (body == NULL) {
@@ -137,44 +180,18 @@ struct __ret_data_t __request(char *cmd) {
         if (nextLine) *nextLine = '\n';  // then restore newline-char, just to be tidy    
         curLine = nextLine ? (nextLine+1) : NULL;
     }
-    // char buf[BUFSIZE];
-    // FILE *fp;
 
-    // if ((fp = popen(cmd, "r")) == NULL) {
-    //     #ifdef DEBUG
-    //     printf("Error opening pipe!\n");
-    //     #endif
-    //     return ret;
-    // }
-
-    // while (fgets(buf, BUFSIZE, fp) != NULL) {
-    //     char* value = buf;
-    //     if (__str_starts_with(buf, "state")) {
-    //         value += strlen("state") + 1;
-    //         strcpy(ret.state, value);
-    //     } else if (__str_starts_with(buf, "value")) {
-    //         value += strlen("value") + 1;
-    //         strcpy(ret.value, value);
-    //     } else if (__str_starts_with(buf, "pin_name")) {
-    //         value += strlen("pin_name") + 1;
-    //         strcpy(ret.pin_name, value);
-    //     } else if (__str_starts_with(buf, "pin_number")) {
-    //         value += strlen("pin_number") + 1;
-    //         ret.pin_number = atoi(value);
-    //     }
-    // }
-
-    // if (pclose(fp)) {
-    //     #ifdef DEBUG
-    //     printf("Command not found or exited with error status\n");
-    //     #endif
-    //     return ret;
-    // }
-    //printf("strlen value:%d|%d\n", strlen(ret.value), ret.value[0]);
     return ret;
 }
 
+// =====
+// Actual wiringPi function implementations
+// =====
+
+uint64_t start_time_us;
+
 int wiringPiSetupGpio(void) {
+    start_time_us = __get_sys_time();
     return 0;
 }
 
@@ -185,7 +202,7 @@ void pinMode(int pin, int pud) {
 void digitalWrite(int pin, int value) {
     char pin_name[16];
     sprintf(pin_name, "GPIO%d", pin);
-    char req_str_f[BUFSIZE];
+    char req_str_f[512];
     sprintf(req_str_f, PATH_SET, pin_name, value);
     __request(req_str_f);
 }
@@ -193,17 +210,32 @@ void digitalWrite(int pin, int value) {
 int digitalRead(int pin) {
     char pin_name[16];
     sprintf(pin_name, "GPIO%d", pin);
-    char req_str_f[BUFSIZE];
+    char req_str_f[512];
     sprintf(req_str_f, PATH_GET, pin_name);
     struct __ret_data_t ret = __request(req_str_f);
     return (strcmp(ret.value, "HIGH") == 0) || (strcmp(ret.value, "1") == 0);
 }
 
-void delay(unsigned int how_long_ms) {
-#if defined(_WIN32) || defined(_WIN64)
-    clock_t start_time = clock();
-    while (clock() < (start_time + how_long_ms));
-#elif defined(__linux__) || defined(__linux) || defined(linux)
-    usleep(how_long_ms * 1000);
+void delay(unsigned int howLong) {
+#if defined(PLATFORM__WIN32)
+    Sleep(howLong);
+#elif defined(PLATFORM__LINUX)
+    usleep(howLong * 1000);
 #endif
+}
+
+void delayMicroseconds(unsigned int howLong) {
+#if defined(PLATFORM__WIN32)
+    Sleep(howLong / 1000 + 1); // + 1 is for safety reasons
+#elif defined(PLATFORM__LINUX)
+    usleep(howLong);
+#endif
+}
+
+unsigned int millis(void) {
+    return (unsigned int)((__get_sys_time() - start_time_us) / 1000);
+}
+
+unsigned int micros(void) {
+    return (unsigned int)(__get_sys_time() - start_time_us);
 }
