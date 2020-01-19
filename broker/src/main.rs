7360b7b6ca8a79bbf98296ec29c94a9e3519a73d
Patrick Goldinger <patrick.goldinger.02@gmail.com>
2020-01-19 20:42:35 +0100

    Implement communication via shared mem, UI via tui
    The new UI of the broker will not be in the browser anymore, but in the
    terminal. This step helps to improve performance alot.
    The UI is still missing important features and a nice style, but it does
    its job for now.
    Crates 'crossterm' + 'tui' will handle user input and rendering.
    
    The communication via shared memory causes some unresolved panics, which
    must be looked into.

--- broker/src/main.rs
+++ broker/src/main.rs
@@ -7,18 +7,29 @@
  */
 
 #![allow(dead_code)]
-//#![allow(unused_imports)]
+#![allow(unused_imports)]
 
-extern crate iced;
-
+use std::{
+    io::{stdout, Write},
+    sync::mpsc,
+    thread,
+    time::Duration,
+};
 use utils::{
     gpioregs::RegMemory,
     shared_memory::*
 };
-use iced::{
-    button, Align, Application, Background, Button, Color, Column, Command,
-    Container, Element, HorizontalAlignment, Length, Row, Settings, Text,
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, Borders, Paragraph, Text, Widget};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen},
 };
+use crossterm::terminal::LeaveAlternateScreen;
 
 const PROJECT_NAME: &str = "SimPi";
 const APP_NAME: &str = "SimPi Broker";
@@ -26,143 +37,151 @@ const VERSION: &str = "0.1.0";
 
 static GLOBAL_LOCK_ID: usize = 0;
 
-pub fn main() {
-    Broker::run(Settings::default());
+enum BrokerEvent<I> {
+    Input(I),
+    Tick,
 }
 
 struct Broker {
-    //theme: style::Theme,
-    reg_memory: Result<SharedMem, SharedMemError>,
-    state_info: button::State,
-    state_settings: button::State,
-    state_pause: button::State,
-    state_reset: button::State,
-    state_terminate: button::State,
+    tick_rate: u64,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    //ThemeChanged(style::Theme),
-    Info,
-    Settings,
-    Pause,
-    Reset,
-    Terminate,
+fn reg_to_bin_str(reg: u32) -> String {
+    let mut ret = String::new();
+    for i in 0..32 {
+        ret = (if (reg & (1 << i)) > 0 { " 1 " } else { " 0 " }).to_owned() + &ret;
+    }
+    return ret;
 }
 
+pub fn main() -> Result<(), failure::Error> {
+    let broker = Broker {
+        tick_rate: 50,
+    };
 
+    enable_raw_mode()?;
 
-impl Application for Broker {
-    type Message = Message;
+    let mut stdout = stdout();
+    execute!(stdout, EnterAlternateScreen)?;
 
-    fn new() -> (Broker, Command<Message>) {
-        (
-            Broker {
-                //theme: style::Theme::Light,
-                reg_memory: utils::init_shared_memory(),
-                state_info: button::State::default(),
-                state_settings: button::State::default(),
-                state_pause: button::State::default(),
-                state_reset: button::State::default(),
-                state_terminate: button::State::default(),
-            },
-            Command::none()
-        )
-    }
+    let backend = CrosstermBackend::new(stdout);
 
-    fn title(&self) -> String {
-        String::from(APP_NAME)
-    }
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
 
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            //Message::ThemeChanged(theme) => self.theme = theme,
-            Message::Terminate => {  },
-            _ => {}
-        }
+    // Setup input handling
+    let (tx, rx) = mpsc::channel();
 
-        Command::none()
-    }
-
-    fn view(&mut self) -> Element<Message> {
-        enum ButtonColor {
-            Red,
-            Grey,
-        }
-        let button = |state, label, button_color| {
-            Button::new(state, Text::new(label))
-                .background(Background::Color(match button_color {
-                    ButtonColor::Red => Color {r:1.0,g:0.0,b:0.0,a:1.0},
-                    ButtonColor::Grey => Color {r:0.7,g:0.7,b:0.7,a:1.0}
-                }))
-                .border_radius(4)
-                .padding(8)
-        };
-        let row_header_quickpanel = Row::new()
-            .spacing(8)
-            .push(
-                button(&mut self.state_info, "i", ButtonColor::Grey)
-                    .on_press(Message::Info)
-            )
-            .push(
-                button(&mut self.state_settings, "s", ButtonColor::Grey)
-                    .on_press(Message::Settings)
-            )
-            .push(
-                button(&mut self.state_pause, "p", ButtonColor::Grey)
-                    .on_press(Message::Pause)
-            )
-            .push(
-                button(&mut self.state_reset, "r", ButtonColor::Red)
-                    .on_press(Message::Reset)
-            )
-            .push(
-                button(&mut self.state_terminate, "t", ButtonColor::Red)
-                    .on_press(Message::Terminate)
-            );
-        let row_header = Row::new()
-            .padding(8)
-            .spacing(400)
-            .push(
-                Text::new(PROJECT_NAME)
-                    .size(32)
-            )
-            .push(row_header_quickpanel);
-        
-        let row_gpioregs = match &self.reg_memory {
-            Ok(v) => {
-                let gpioregs = v.rlock::<RegMemory>(GLOBAL_LOCK_ID).unwrap();
-                Row::new()
-                    .push(
-                        Text::new(gpioregs.output.read_to_str())
-                            .size(16)
-                    )
-            },
-            Err(err) => {
-                Row::new()
-                    .push(
-                        Text::new(format!("{}", err))
-                            .size(16)
-                    )
+    thread::spawn(move || {
+        loop {
+            // poll for tick rate duration, if no events, sent tick event.
+            if event::poll(Duration::from_millis(broker.tick_rate)).unwrap() {
+                if let Event::Key(key) = event::read().unwrap() {
+                    tx.send(BrokerEvent::Input(key)).unwrap();
+                }
             }
-        };
+            tx.send(BrokerEvent::Tick).unwrap();
+        }
+    });
 
-        let row_footer = Row::new()
-            .push(
-                Text::new(VERSION)
-                    .size(16)
-            );
-        
-        Container::new(
-            Column::new()
-                .width(Length::Fill)
-                .height(Length::Fill)
-                .push(row_header)
-                .push(row_gpioregs)
-                .push(row_footer)
-        )
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .into()
+    terminal.clear()?;
+
+    let reg_memory = utils::init_shared_memory();
+
+    loop {
+        terminal.draw(|mut f| {
+            let body_layout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Length(9),
+                        Constraint::Length(3),
+                    ].as_ref()
+                )
+                .split(f.size());
+            Block::default()
+                .title(" SimPi Broker ")
+                .borders(Borders::ALL)
+                .render(&mut f, body_layout[0]);
+            
+            let gpioregs_layout = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Length(10),
+                        Constraint::Min(1),
+                    ].as_ref()
+                )
+                .split(body_layout[1]);
+            let gpioregs_names = [
+                Text::raw("\n"),
+                Text::raw("INPUT\n"),
+                Text::raw("OUTPUT\n"),
+                Text::raw("CONFIG\n"),
+                Text::raw("INTEN\n"),
+                Text::raw("INT0\n"),
+                Text::raw("INT1\n"),
+            ];
+            let gpioregs_data = match reg_memory.as_ref() {
+                Ok(v) => {
+                    let reg_memory = v.rlock::<RegMemory>(GLOBAL_LOCK_ID).unwrap();
+                    [
+                        Text::styled(
+                            "31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 09 08 07 06 05 04 03 02 01 00 \n",
+                            Style::default().fg(Color::DarkGray)
+                        ),
+                        Text::raw(reg_to_bin_str(reg_memory.input.read()) + "\n"),
+                        Text::raw(reg_to_bin_str(reg_memory.output.read()) + "\n"),
+                        Text::raw(reg_to_bin_str(reg_memory.config.read()) + "\n"),
+                        Text::raw(reg_to_bin_str(reg_memory.inten.read()) + "\n"),
+                        Text::raw(reg_to_bin_str(reg_memory.int0.read()) + "\n"),
+                        Text::raw(reg_to_bin_str(reg_memory.int1.read()) + "\n"),
+                    ]
+                },
+                Err(err) => {
+                    [
+                        Text::raw(format!("{}", err)),
+                        Text::raw(""),
+                        Text::raw(""),
+                        Text::raw(""),
+                        Text::raw(""),
+                        Text::raw(""),
+                        Text::raw(""),
+                    ]
+                }
+            };
+            let gpioregs_block = Block::default()
+                .borders(Borders::NONE);
+            Block::default()
+                .title(" GPIO Registers ")
+                .borders(Borders::ALL)
+                .render(&mut f, body_layout[1]);
+            Paragraph::new(gpioregs_names.iter())
+                .block(gpioregs_block.clone())
+                .alignment(Alignment::Left)
+                .render(&mut f, gpioregs_layout[0]);
+            Paragraph::new(gpioregs_data.iter())
+                .block(gpioregs_block.clone())
+                .alignment(Alignment::Right)
+                .render(&mut f, gpioregs_layout[1]);
+        }).unwrap();
+        match rx.recv()? {
+            BrokerEvent::Input(event) => {
+                match event.code {
+                    KeyCode::Char('q') => {
+                        disable_raw_mode()?;
+                        execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+                        terminal.show_cursor()?;
+                        break;
+                    },
+                    _ => {}
+                }
+            },
+            BrokerEvent::Tick => {},
+            //_ => {}
+        }
     }
+    Ok(())
 }
